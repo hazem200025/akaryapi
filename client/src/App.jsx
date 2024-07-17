fd5bb26a3ea99461cfa617ae4631801cadac845b
hazem200025 <153329457+hazem200025@users.noreply.github.com>
2024-07-17 02:11:37 +0200

    fix update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage";
-import UpdatePage from "./routes/updatePage/updatePage";
+import UpdatePage from "./routes/updatePage/UpdatePage";
 import RequestPage  from "./routes/requestPage/RequestPage";
 import DetailPage from './routes/detailpage/DetailPage';
 import Contaus from './routes/contactus/contactus';
